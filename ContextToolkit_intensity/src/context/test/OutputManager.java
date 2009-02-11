ed79f2a8ffcada3c6316d934cf7c3bfe633c0b88
dragonwanghuai <dragonwanghuai@d86303fe-d71a-11dd-b6b4-918b84bbd919>
2009-02-11 12:59:27 +0000

    Context intensity: OutputManager was added to tune the program

---
+++ ContextToolkit_intensity/src/context/test/OutputManager.java
@@ -0,0 +1,114 @@
+package context.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import context.test.util.Constant;
+import context.test.util.Logger;
+
+public class OutputManager {
+
+	/**get the list of failed test cases for golden versions, save it
+	 * to a specified file 
+	 * 
+	 * @param saveFile
+	 */
+	public void saveFailedTestCase(String saveFile){
+		
+		StringBuffer sb = new StringBuffer();
+		File sourceDir = new File(Constant.baseFolder + "/test/output/FailureRate/0");		
+		File[] outputs = sourceDir.listFiles();
+		for(File output: outputs){
+			if(output.length() > 0){
+				String name = output.getName();
+				sb.append(name.substring(0, name.indexOf("."))+"\t");
+			}
+		}
+		
+		Logger.getInstance().setPath(saveFile, false);
+		Logger.getInstance().write(sb.toString());
+		Logger.getInstance().close();
+	}
+	
+	public String[] getFailedTestCase(String saveFile){
+		String[] list = null;
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(saveFile));
+			StringBuilder sb = new StringBuilder();
+			String str = null;
+			while((str = br.readLine())!=null){
+				sb.append(str);
+			}
+			
+			str = sb.toString();
+			list = str.split("\t");  
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	
+	/**Compare the stability of faults detected by test cases  
+	 * 
+	 * @param srcPath: contain less files
+	 * @param destPath: contain more files
+	 * @return
+	 */
+	public ArrayList changeList(String srcPath, String destPath){
+		ArrayList diffList = new ArrayList();
+
+		File[] srcDir = new File(srcPath).listFiles();
+		
+		File[] destDir = new File(destPath).listFiles();		
+		ArrayList destList = new ArrayList();
+		for(File file: destDir)
+			destList.add(file.getName());
+		
+		int i = 0; 
+		for( ; i < srcDir.length; i++){
+			File file = srcDir[i];
+			int index = destList.indexOf(file.getName());
+			if(index > -1){
+				long srcLength = file.length();
+				long destLength = destDir[index].length();
+				if(srcLength!=destLength){
+					diffList.add(file.getName());
+				}
+			}
+		}
+		
+			
+		return diffList;
+	}
+	
+	public static void main(String[] args){
+		OutputManager manager = new OutputManager();
+//		String saveFile = Constant.baseFolder + "/test/output/failedTestCase.txt";
+//		manager.getFailedTestCase(saveFile);
+//		String[] strs = manager.getFailedTestCase(saveFile);
+//		System.out.println("0");
+
+		String srcPath = Constant.baseFolder + "/test/output/FailureRate/backup/oracle_100";
+		String destPath = Constant.baseFolder + "/test/output/FailureRate/0";
+		
+		StringBuilder sb = new StringBuilder();
+		ArrayList changeList = manager.changeList(srcPath, destPath);
+		sb.append("change list:" + changeList.size()+"\n");
+		
+		for(Object file: changeList){
+			sb.append(file+"\n");
+		}
+		
+	}
+}
--- ContextToolkit_intensity/src/context/test/TestManagers.java
+++ ContextToolkit_intensity/src/context/test/TestManagers.java
@@ -4,6 +4,7 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -18,6 +19,22 @@ public class TestManagers {
 
 	public TestSetManager manager_TS;
 	public Manipulator manager;
+
+	
+	public void getOutput( int iteration){
+		OutputManager manager_OP = new OutputManager();
+		String saveFile = Constant.baseFolder + "/test/output/failedTestCase.txt";
+		manager_OP.getFailedTestCase(saveFile);
+		String[] testCaseList = manager_OP.getFailedTestCase(saveFile);
+
+		for(String testCase: testCaseList){
+			int testCaseIndex = Integer.parseInt(testCase);
+			Vector testPool = new Vector();
+			testPool.add(testCaseIndex);
+			this.getOutput(0, 0, testPool, iteration);
+		}
+	}
+	
 	
 	
 	/**2009/2/11: due to the non-deterministic execution of concurrent programs,
@@ -28,18 +45,26 @@ public class TestManagers {
 	 * @param max_Version
 	 * @param testPool
 	 * @param iteration
+	 * @param min_TestCase
+	 * @param max_TestCase
 	 */
 	public void getOutput(int min_Version, int max_Version, Vector testPool,
-			int iteration) {
+			int iteration, int min_TestCase, int max_TestCase) {
 		for (int versionNumber = min_Version; versionNumber <= max_Version; versionNumber++) {
-			for (int i = 0; i < testPool.size(); i++) {
+			
+			//when multiple threads it is dangerous to delete it
+			String directory = Constant.baseFolder
+			+ "test/output/FailureRate/" + versionNumber + "/";
+//			Logger.getInstance().delete(directory);
+			if(!new File(directory).exists()){
+				new File(directory).mkdirs();	
+			}
+			
+			
+			for (int i = min_TestCase; i <= max_TestCase; i++) {
 				// 1.prepare the directory: delete it when it exists and
 				// recreate it
-				String directory = Constant.baseFolder
-						+ "test/output/FailureRate/" + versionNumber + "/";
-				Logger.getInstance().delete(directory);
-				new File(directory).mkdirs();
-
+				
 				String testCaseInstance = (String) testPool.get(i);
 				String file = directory + "/" + i + ".txt";
 
@@ -52,9 +77,20 @@ public class TestManagers {
 				for (int k = 0; k < iteration; k++) {
 					try {
 						// 5.redirect the output before execution
-						PrintStream ps = new PrintStream(
-								new BufferedOutputStream(new FileOutputStream(
-										file, true)));
+						FileOutputStream outStr = new FileOutputStream(file, true);
+						BufferedOutputStream bufStr = new BufferedOutputStream(outStr);
+						PrintStream ps = new PrintStream(bufStr);
+						
+//						if(new File(file).exists()){
+//							ps = new PrintStream(
+//									new BufferedOutputStream(new FileOutputStream(
+//											file, true)));
+//						}else{
+//							ps = new PrintStream(
+//									new BufferedOutputStream(new FileOutputStream(
+//											file, false)));
+//						}
+						
 						System.setOut(ps);
 						System.setErr(ps);
 						// 6. execute the test case
@@ -64,79 +100,39 @@ public class TestManagers {
 						PositionIButton.getInstance().stopRunning();
 
 						// 7. close the output
+						outStr.close();
+						bufStr.close();
 						ps.close();
 
 					} catch (FileNotFoundException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
+					} catch (IOException ex) {
+						ex.printStackTrace();
 					}
 				}
 			}
 		}
 	}
-		
 
 	
 	
-	public void executeTestSets(int min_Version, int max_Version, String criteria, Vector testSets, int iteration){
-		for (int versionNumber = min_Version; versionNumber <= max_Version; versionNumber++) {
-			for (int i = 0; i < testSets.size(); i++) {
-				
-				// 1.prepare the directory: delete it when it exists and
-				// recreate it
-				String directory = Constant.baseFolder + "test/output/"
-						+ criteria + "/" + versionNumber + "/" + i;
-				Logger.getInstance().delete(directory);
-				new File(directory).mkdirs();
-
-				Vector testSet = (Vector) testSets.get(i);
-				for (int j = 0; j < testSet.size(); j++) {
-					int testCaseIndex = Integer.parseInt((String) testSet
-							.get(j));
-					String testCaseInstance = manager_TS
-							.getTestCaseInstance(testCaseIndex);
-					String file = directory + "/" + testCaseIndex + ".txt";
-//					 System.err.println("versionNumber:" + versionNumber +
-//					 "\n"
-//					 + "testSetNumber:" + i + "\n" +
-//					 "testCaseNumber:" + testCaseIndex + "\n" + file + "\n");
-					
-					for(int k = 0; k < iteration; k++){
-						try {
-							// 5.redirect the output before execution
-							PrintStream ps = new PrintStream(
-									new BufferedOutputStream(new FileOutputStream(
-											file, true)));
-							System.setOut(ps);
-							System.setErr(ps);
-							// 6. execute the test case
-							PositionIButton.getInstance().set(0, testCaseIndex,
-									testCaseInstance);
-							PositionIButton.getInstance().runTestCase();
-							PositionIButton.getInstance().stopRunning();
-
-							// 7. close the output
-							ps.close();
-							// 8. reset the drivers
-
-						} catch (FileNotFoundException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
 	
-					}
-				}
-				if(!criteria.equals("None")){
-					manager = Manipulator.getInstance(Constant.baseFolder
-							+ "ContextIntensity/Drivers/Drivers_"+ criteria + ".txt"); // reinitial it for each test set	
-				}
-//				manager.setDrivers(drivers);
-			}
-		}
-		
+	public void getOutput(int min_Version, int max_Version, Vector testPool,
+			int iteration) {
+		this.getOutput(min_Version, max_Version, testPool, iteration, 0, testPool.size()-1);
 	}
 	
-	
+	/**get failure rate of each faulty version, and list all failed test cases to expose it(context intensity)
+	 * 
+	 * @param min_Version
+	 * @param max_Version
+	 */
+	public void getFailureRate(int min_Version, int max_Version){
+		
+		
+	}
+		
 	public void executeTestSets(int min_Version, int max_Version,
 			String criteria, Vector testSets) {
 
@@ -209,13 +205,39 @@ public class TestManagers {
 		String testPoolFile = Constant.baseFolder + "ContextIntensity/TestPool.txt";
 		test.manager_TS.loadTestPoolFromFile(testPoolFile);
 		Vector testPool = test.manager_TS.testPool;
-		int min_Version = 0;
-		int max_Version = 10;
-		int iteration = 100;
-		long start = System.currentTimeMillis();
-		test.getOutput(min_Version, max_Version, testPool, iteration);
-		System.out.println(System.currentTimeMillis()-start);
 		
+		int min_Version, max_Version, iteration;
+		
+		if(args.length ==0){ //slowest but most automated
+			min_Version = 0;
+			max_Version = 236;
+			iteration = 100;
+			
+			long start = System.currentTimeMillis();
+			test.getOutput(min_Version, max_Version, testPool, iteration);
+			System.out.println(System.currentTimeMillis()-start);
+		}else if(args.length == 1){
+			iteration = Integer.parseInt(args[0]);
+			test.getOutput(iteration);
+		}else if(args.length ==3){
+			min_Version = Integer.parseInt(args[0]);
+			max_Version = Integer.parseInt(args[1]);
+			iteration = Integer.parseInt(args[2]);
+			
+			long start = System.currentTimeMillis();
+			test.getOutput(min_Version, max_Version, testPool, iteration);
+			System.out.println(System.currentTimeMillis()-start);
+			
+		}else if(args.length == 5){ 
+			//fasted, require about 40 minutes to generate 1000*100 outputs if 10 threads are opened
+			min_Version = Integer.parseInt(args[0]);
+			max_Version = Integer.parseInt(args[1]);
+			iteration = Integer.parseInt(args[2]);
+			
+			int min_TestCase = Integer.parseInt(args[3]);
+			int max_TestCase = Integer.parseInt(args[4]);
+			test.getOutput(min_Version, max_Version, testPool, iteration, min_TestCase, max_TestCase);
+		}
 		
 		// 1. get Adequacy test sets from files
 //		String file_TS = Constant.baseFolder
