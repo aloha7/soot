7e0cbb0557aa2f6c2e5bb955ca54438165352223
dragonwanghuai <dragonwanghuai@d86303fe-d71a-11dd-b6b4-918b84bbd919>
2009-03-30 01:32:07 +0000

    Add two extra methods: genTestSet() and exeTestSet(), which is used to generate and execute specified test setss

--- Franklin_parserTool/src/ccr/test/ScriptManager.java
+++ Franklin_parserTool/src/ccr/test/ScriptManager.java
@@ -1,10 +1,10 @@
 package ccr.test;
 
-import java.util.Iterator;
-import java.util.Vector;
-
-import ccr.stat.CFG;
-import ccr.stat.Criterion;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ScriptManager {
 	
@@ -47,7 +47,7 @@ public class ScriptManager {
 		
 		StringBuilder sb = new StringBuilder();
 		for(int testSuiteSize = min_TestSuiteSize; testSuiteSize < max_TestSuiteSize; testSuiteSize ++){
-			sb.append("java ccr.test.ExecutionManager Load "+ testSuiteSize + " " + date + " \n");
+			sb.append("java -Xmx1500m ccr.test.ExecutionManager Load "+ testSuiteSize + " " + date + " \n");
 //			if (testSuiteSize % interval == 0) { // sleep a fixed time if a specified
 //				// interval is reached
 //				sb.append("sleep " + sleep_sed + "\n");
@@ -120,7 +120,7 @@ public class ScriptManager {
 				
 				//2009-03-07: have no parameters to fix, but we need 20 shells to run at the same time
 				//and for each shell, all commands should execute sequentially.
-				sb.append("java ccr.test.ExecutionManager "+ testSetNum + " " + instruction + " " 
+				sb.append("java -Xmx1500m ccr.test.ExecutionManager "+ testSetNum + " " + instruction + " " 
 						+ min_CI + " " + max_CI + " " + date + " " + criterion + " " + testSuiteSize
 						+" " + oldOrNew + " " + randomOrCriterion +" \n");
 			}
@@ -159,7 +159,7 @@ public class ScriptManager {
 			sb.append("java ccr.test.ExecutionManager " + testSetNum + " " + instruction + " "
 					+ min_CI + " " + max_CI + " " + date + " " + criterion
 					+ " -1 " + oldOrNew + " random"
-					+ " &\n");
+					+ " \n");
 		}
 
 		return sb.toString();
@@ -200,6 +200,59 @@ public class ScriptManager {
 		return sb.toString();
 	}
 	
+	public static String exeTestSet(String criterionString, int testSetNum, String instruction, double min_CI,
+			double max_CI, String date, int testSuiteSize){
+		StringBuffer sb = new StringBuffer();
+		
+		String[] strs = criterionString.split("_");
+
+		String criterion = strs[0];
+		if(criterion.equals("RandomTestSets")){
+			sb.append("java -Xmx1500m ccr.test.ExecutionManager "+ instruction +" " 
+					+testSuiteSize + " " + date + " \n");			
+		}else{
+			criterion = strs[0].substring(0, strs[0].indexOf("TestSets"));
+			String oldOrNew = strs[1];
+			String randomOrCriterion = strs[2];
+			
+			sb.append("java -Xmx1500m ccr.test.ExecutionManager "+ testSetNum + " " + instruction + " " 
+					+ min_CI + " " + max_CI + " " + date + " " + criterion + " " + testSuiteSize
+					+" " + oldOrNew + " " + randomOrCriterion +" \n");	
+		}
+		
+		
+		
+		return sb.toString();
+	}
+	
+	public static String genTestSet(String criterionString, int testSetNum, double min_CI,
+			double max_CI, String date, int testSuiteSize) {
+
+		StringBuilder sb = new StringBuilder();
+		
+		String[] strs = criterionString.split("_");
+		String criterion = strs[0];
+		if(criterion.equals("RandomTestSets")){
+			sb.append("java ccr.test.TestSetManager " + testSetNum + " " + testSuiteSize
+					+ " " + date + " \n");
+		}else{
+			criterion = strs[0].substring(0, strs[0].indexOf("TestSets"));
+			String oldOrNew = strs[1];
+			String randomOrCriterion = strs[2];
+			sb.append("java ccr.test.TestSetManager " + testSetNum
+					+ " Context_Intensity " + min_CI + " " + max_CI + " "
+					+ date + " " + criterion + " " + testSuiteSize + " " + oldOrNew
+					+ " " + randomOrCriterion + " \n");
+		}
+		
+		return sb.toString();
+		
+	}
+
+
+		
+		
+	
 	/**
 	 * To generate random test sets with fixed size
 	 * 
@@ -219,7 +272,7 @@ public class ScriptManager {
 		
 		for (int i = min_TestSuiteSize; i < max_TestSuiteSize; i++) {
 			sb.append("java ccr.test.TestSetManager " + testSetNum + " " + i
-					+ " " + date + " \n");
+					+ " " + date + " &\n");
 //			if (i % interval == 0) { // sleep a fixed time if a specified
 //				// interval is reached
 //				sb.append("sleep " + sleep_sed + "\n");
@@ -310,7 +363,7 @@ public class ScriptManager {
 				sb.append("java ccr.test.TestSetManager " + testSetNum
 						+ " Context_Intensity " + min_CI + " " + max_CI + " "
 						+ date + " " + criterion + " " + i + " " + oldOrNew
-						+ " " + randomOrCriterion + " \n");
+						+ " " + randomOrCriterion + " &\n");
 				
 			}
 		}
@@ -367,6 +420,25 @@ public class ScriptManager {
 		return sb.toString();
 	}
 
+	public static void genFailureRate_Sequential(String date, String saveFile, 
+			ArrayList faultList, int startVersion, int endVersion){
+		StringBuilder sb = new StringBuilder();
+		for(int i = startVersion; i < endVersion-1; i ++){
+			sb.append("java ccr.test.TestDriver getFailureRate " + date + " " + faultList.get(i) + " "
+					+ faultList.get(i+1) + " a\n");
+		}
+		
+		if(startVersion == (endVersion - 1)){ //the last element in the faultList
+			int version = (Integer)faultList.get(startVersion);
+			sb.append("java ccr.test.TestDriver getFailureRate " + date + " " + version + " "
+					+ (version + 1) + " a\n");
+		}
+		
+		Logger.getInstance().setPath(saveFile, false);
+		Logger.getInstance().write(sb.toString());
+		Logger.getInstance().close();
+	}
+	
 	/**2009-03-17: we must execute these faulty version one by one since some faults may 
 	 * cause program crashed
 	 * @param date
@@ -466,11 +538,12 @@ public class ScriptManager {
 			ScriptManager.save(exeAdequateTS_Script(testSetNum, instruction, min_CI, max_CI, 
 					date, min_TestSuiteSize, max_TestSuiteSize), saveFile);
 			
-		}else if(instruction.equals("AllInOne")){
+		}else if(instruction.equals("AllInOne")){//getSizePerformance
+			//This scripts is used to investigate the correlations between test suite size and testing performances
 			//generate all random, adequate test sets, then execute them and analyze them
 			//2009-03-07: we set a interval to separate all tasks into several shells
-			int interval = 5;
-			int start = 127; //(1-127:7, 127-200:5)
+			int interval = 30;
+			int start = 80; //(1-127:7, 127-200:5); 20090323:(1-80:40, 80-200:30)
 			int end = 200;
 			
 			StringBuilder sb1 = new StringBuilder();
@@ -489,11 +562,11 @@ public class ScriptManager {
 					+"_"+max_TestSuiteSize+".sh &" + "\n");
 				
 				StringBuilder sb = new StringBuilder();
-				sb.append(genRandomTS_Script(testSetNum, min_TestSuiteSize,
-						max_TestSuiteSize, date));
-				
-				sb.append(genAdequateTS_Script(testSetNum, min_CI, max_CI,
-						date, min_TestSuiteSize, max_TestSuiteSize));
+//				sb.append(genRandomTS_Script(testSetNum, min_TestSuiteSize,
+//						max_TestSuiteSize, date));
+//				
+//				sb.append(genAdequateTS_Script(testSetNum, min_CI, max_CI,
+//						date, min_TestSuiteSize, max_TestSuiteSize));
 				
 				sb.append(exeRandomTS_Script(min_TestSuiteSize, 
 						max_TestSuiteSize, date));
@@ -509,6 +582,70 @@ public class ScriptManager {
 			
 //			sb1.append("java ccr.test.ResultAnalyzer getSizePerformance " + date + "\n");
 			ScriptManager.save(sb1.toString(), saveFile);
+		}else if(instruction.equals("AllInOne_Residual")){
+			//2009-03-29: an method to handle residual tests
+			String srcDir = "src/ccr/experiment/Context-Intensity_backup/TestHarness/" + date;
+			
+			//load residual test sets to execute
+			String lostTestFile = srcDir +"//LostTest.txt";
+			boolean containHeader = false;
+			String line = null;
+			
+			StringBuilder sb = new StringBuilder();
+			
+			int interval = 23;
+			
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(lostTestFile));
+				if(containHeader)
+					br.readLine();
+				
+				//2.generate the test set execution scripts
+				instruction = "Load";
+				
+				int counter = 0; //count the script files generated
+				int count = 1;
+				while((line = br.readLine())!=null){
+					String[] strs = line.split("\t");
+					String criterionString = strs[0];
+					for(int i = 1; i < strs.length; i ++){
+						int testSuiteSize = Integer.parseInt(strs[i]);
+						sb.append(ScriptManager.genTestSet(criterionString, testSetNum, min_CI, max_CI, date, testSuiteSize));
+						sb.append(ScriptManager.exeTestSet(criterionString, testSetNum, instruction, min_CI, max_CI, date, testSuiteSize));
+						if(count % interval == 0){//save it 
+							saveFile = srcDir + "/Script/ResidualTests_"+ (count - interval)+ "_" + count+".sh";					
+							ScriptManager.save(sb.toString(), saveFile);
+							sb = new StringBuilder();
+							counter ++;
+						}
+						count ++;
+					}
+				}
+				if(sb.toString().length()!=0){//save the last one
+					count = (counter + 1) * interval ;
+					saveFile = srcDir + "/Script/ResidualTests_"+ (count - interval)+ "_" + count+".sh";
+					ScriptManager.save(sb.toString(), saveFile);
+				}
+				//a master script
+				sb = new StringBuilder();
+				count =interval;
+				for(int i =0; i < counter; i ++){					
+					sb.append("./ResidualTests_"+(count-interval) + "_" + count +".sh &\n");
+					count += interval;
+				}
+				
+				saveFile = srcDir + "/Script/ResidualTests.sh";
+				ScriptManager.save(sb.toString(), saveFile);
+				
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 		}else if(instruction.equals("3GroupComparison")){
 			//2009-03-09:we are only interest in comparing three groups of testing criteria
 			saveFile = "src/ccr/experiment/Context-Intensity_backup/TestHarness/"
@@ -517,33 +654,36 @@ public class ScriptManager {
 			StringBuilder sb = new StringBuilder();
 			instruction = "Load";
 			//Group 1
-//			sb.append(genRandomTS_Script(testSetNum, 27,
-//					28,  date)); //to compare AllPolicies			
-//			sb.append(genRandomTS_Script(testSetNum, 43,
-//					44, date)); //to compare All1ResovledDU
-//			sb.append(genRandomTS_Script(testSetNum, 50,
-//					51, date)); //to compare All2ResolvedDU
-//			
-//			sb.append(genAdequateTS_WithoutFixSize_Script(testSetNum, min_CI, max_CI,
-//					date));
+			sb.append(genRandomTS_Script(testSetNum, 27,
+					28,  date)); //to compare AllPolicies			
+			sb.append(genRandomTS_Script(testSetNum, 42,
+					43, date)); //to compare All1ResovledDU
+			sb.append(genRandomTS_Script(testSetNum, 50,
+					51, date)); //to compare All2ResolvedDU			
+			sb.append(genAdequateTS_WithoutFixSize_Script(testSetNum, min_CI, max_CI,
+					date));
+									
+			//Group 2 & Group 3
+			sb.append(genRandomTS_Script(testSetNum, 62,
+					63, date));
+
+			sb.append(genAdequateTS_Script(testSetNum, min_CI, max_CI,
+					date, 62, 63 ));
+			
+			//Execute test sets
 			sb.append(exeRandomTS_Script(27, 
 					28,instruction ,date));
-			sb.append(exeRandomTS_Script(43, 
-					44, instruction,date));
+			sb.append(exeRandomTS_Script(42, 
+					43, instruction,date));
 			sb.append(exeRandomTS_Script(50, 
 					51, instruction,date));
 			sb.append(exeAdequateTS_withoutFixSize_Script(testSetNum, min_CI, max_CI, instruction,
 					date));
 			
-			//Group 2 & Group 3
-//			sb.append(genRandomTS_Script(testSetNum, 62,
-//					63, date));
-//
-//			sb.append(genAdequateTS_Script(testSetNum, min_CI, max_CI,
-//					date, 62, 63 ));
-			
 			sb.append(exeRandomTS_Script(62, 
 					63, instruction,date));
+			
+			
 			
 			sb.append(exeAdequateTS_Script(testSetNum, instruction, min_CI, max_CI, 
 					date, 62, 63));
@@ -605,14 +745,16 @@ public class ScriptManager {
 			
 			
 			int start = 0;
-			int end = 128; //[140, 3600][3600, 5024]
+			int end = 544; //[140, 3600][3600, 5024]
 			int concurrent = 10;
 			
+			//2.concurrent execution
 //			saveFile = "src/ccr/experiment/Context-Intensity_backup/TestHarness/"
 //				+ date + "/Script/GetFailureRate_Executor_"+start + "_" + end +".sh";
 //			
 //			ScriptManager.genFailureRate_TS(start, end, concurrent, date, saveFile);
 			
+			//1. sequential execution
 			int interval = (end - start + 1)/concurrent ;
 			if( (end - start + 1) > interval * concurrent )
 				interval ++;
@@ -639,6 +781,59 @@ public class ScriptManager {
 			saveFile = "src/ccr/experiment/Context-Intensity_backup/TestHarness/"
 				+ date + "/Script/GetFailureRate_Executor_"+start + "_" + end +".sh";
 			ScriptManager.save(sb1.toString(), saveFile);
+			
+			//3. 2009-03-20: residual execution
+//			int[] residualVersions = new int[]{
+//					2341, 2402,
+//					3434, 3446,
+//					1509, 1532,
+//					3090, 3098,
+//					3249, 3272
+//			};
+//			ArrayList faultList = new ArrayList(); 
+//			for(int i = 0; i < residualVersions.length; i = i +2){
+//				int start = residualVersions[i];
+//				int end  = residualVersions[i + 1];
+//				for(int j = start; j < end; j ++){
+//					faultList.add(j);
+//				}
+//			}
+//			
+//			int start = 0;
+//			int end = faultList.size();
+//			int concurrent = 20;
+//			int counter = 0;
+//			
+//			int interval = (end - start )/concurrent ;
+//			if( (end - start) > interval * concurrent )
+//				interval ++;
+//			
+//			
+//			for(int i = start; i < end; i = i + interval){
+//				int startVersion = i;
+//				int endVersion = i + interval + 1;
+//				if(endVersion > end)
+//					endVersion = end;
+//				
+//				instruction = "getFailurRate";
+//				saveFile = "src/ccr/experiment/Context-Intensity_backup/TestHarness/"
+//					+ date + "/Script/GetFailureRate_"+counter +".sh";
+//				
+//				
+//				
+//				StringBuilder sb = new StringBuilder();
+//				ScriptManager.genFailureRate_Sequential(date, saveFile, faultList, startVersion, endVersion);
+//				counter = (counter + 1) % concurrent;
+//			}
+//			
+//			StringBuilder sb1 = new StringBuilder(); 
+//			for(int i = 0; i < counter; i ++){
+//				sb1.append("./GetFailureRate_"+ i + ".sh &" + "\n");
+//			}
+//
+//			saveFile = "src/ccr/experiment/Context-Intensity_backup/TestHarness/"
+//				+ date + "/Script/GetFailureRate_Executor_"+start + "_" + end +".sh";
+//			ScriptManager.save(sb1.toString(), saveFile);
 			
 		}
 	}
