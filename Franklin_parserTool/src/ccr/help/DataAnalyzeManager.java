1efeb9236d88dff7eb86472ce6bd8f8db34a7743
DragonWangHuai <DragonWangHuai@d86303fe-d71a-11dd-b6b4-918b84bbd919>
2010-03-16 06:30:08 +0000

    2010-03-16:1).can get the description of a double array; 2).implement an algorithm to get the percentile of an array

--- Franklin_parserTool/src/ccr/help/DataAnalyzeManager.java
+++ Franklin_parserTool/src/ccr/help/DataAnalyzeManager.java
@@ -1,7 +1,10 @@
 package ccr.help;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DataAnalyzeManager {	
-	
 	
 	public static CurveFittingResult getLinearCurveFitting(double[] x, double[] y){
 		double sum_x = 0.0;
@@ -58,11 +61,96 @@ public class DataAnalyzeManager {
 		return PearsonCorrelationCoefficient;
 	}
 	
+	/**2010-03-15: return a percentile value for a given data array and a percentile
+	 * 
+	 * @param data
+	 * @param percentile: 50 means 50% percentile (median)
+	 * @return
+	 */
+	public static double getPercentileValue(double[] data, int percentile){
+		
+		double percentileValue = 0.0;
+		
+		Arrays.sort(data); //sort data in ascending order
+		double size = data.length;
+		double rank = percentile*(size + 1)/100;
+		
+		String rankStr = new DecimalFormat("0.00").format(rank);
+		int index = rankStr.indexOf(".");
+		int intergerPortion = Integer.parseInt(rankStr.substring(0, index));
+		int fractionalPortion = Integer.parseInt(rankStr.substring(index+1));
+		if(fractionalPortion == 0){
+			percentileValue = data[intergerPortion-1];
+		}else{
+			//interpolate 
+			double value1 = data[intergerPortion-1];
+			double value2 = data[intergerPortion];
+			
+			double percentile_temp = ((double)fractionalPortion/100) * (value2 - value1) + value1;
+			for(int i = 0; i < size; i ++){ //find the smallest value greater than or equal to percentile_temp
+				double ins = data[i];
+				if(ins >= percentile_temp){
+					percentileValue = ins;
+					break;
+				}
+			}
+		}
+		
+		return percentileValue;
+	}
+	
+	/**2010-03-14:get the min, max, mean, median, std description of the data Array
+	 * 
+	 * @param data: 
+	 * @return
+	 */
+	public static DataDescriptionResult getDataDescriptive(double[] data){
+		DataDescriptionResult descriptionResult = new DataDescriptionResult();
+		
+		double min = data[0];
+		double max = data[data.length - 1];
+		double sum = 0.0;
+		
+		for(int i =0; i < data.length; i++){
+			double ins = data[i];
+			sum += ins;											
+		}
+		
+		double mean = sum/data.length;
+		
+		double temp = 0.0;
+		for(int i = 0; i < data.length; i ++){
+			double ins = data[i];
+			temp += (ins - mean)*(ins-mean);
+		}
+		double std = Math.sqrt(temp/data.length);
+		double median = getPercentileValue(data, 50);
+		
+		descriptionResult.min = min;
+		descriptionResult.max = max;
+		descriptionResult.mean = mean;
+		descriptionResult.median = median;
+		descriptionResult.sum = sum;
+		descriptionResult.std = std;
+		
+		return descriptionResult;
+	}
+	
 	public static void main(String[] args){
 		double[] x = new double[]{3, 5, 9};
 		double[] y = new double[]{8, 11, 14};
 		
-		System.out.println(DataAnalyzeManager.getPearsonCorrelationTest(x, y));
+//		System.out.println(DataAnalyzeManager.getPearsonCorrelationTest(x, y));
+		
+//		x = new double[]{5, 3, 7, 9, 8, 11, 13, 15};
+		
+//		x = new double[]{4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 7,7, 8, 8, 9, 9, 9, 10, 10, 10};
+		
+		x = new double[]{2, 3, 5, 9, 11};
+		
+		System.out.println(getPercentileValue(x, 50));
 		
 	}
+	
+	
 }
