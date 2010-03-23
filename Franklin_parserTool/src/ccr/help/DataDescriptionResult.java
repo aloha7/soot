e2ac14aa05294cab1ae2fa8ab987eb0be7262511
DragonWangHuai <DragonWangHuai@d86303fe-d71a-11dd-b6b4-918b84bbd919>
2010-03-23 09:59:54 +0000

    2010-03-23: a wrapper class to keep data description results

---
+++ Franklin_parserTool/src/ccr/help/DataDescriptionResult.java
@@ -0,0 +1,11 @@
+package ccr.help;
+
+public class DataDescriptionResult {
+	public double min;
+	public double max;
+	public double median;
+	public double mean;
+	public double std;
+	
+	public double sum;
+}
