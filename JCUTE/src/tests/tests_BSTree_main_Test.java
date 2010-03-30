f1183411e8cc82da8b8ac114eb1e7fff2088a52c
DragonWangHuai <DragonWangHuai@d86303fe-d71a-11dd-b6b4-918b84bbd919>
2010-03-30 06:47:34 +0000

    2010-03-30: JCUTE: a concolic test case generation tool for Java programs

---
+++ JCUTE/src/tests/tests_BSTree_main_Test.java
@@ -0,0 +1,155 @@
+package tests;
+
+/* JUnit test case generated automatically by CUTE */
+import junit.framework.*;
+
+public class tests_BSTree_main_Test extends TestCase implements cute.Input {
+    private Object[] input;
+    private int i;
+
+    public tests_BSTree_main_Test(String name){
+        super(name);
+    }
+
+    public boolean Boolean() {
+        return ((Boolean)input[i++]).booleanValue();
+    }
+
+    public short Short() {
+        return ((Short)input[i++]).shortValue();
+    }
+
+    public int Integer() {
+        return ((Integer)input[i++]).intValue();
+    }
+
+    public long Long() {
+        return ((Long)input[i++]).longValue();
+    }
+
+    public float Float() {
+        return ((Float)input[i++]).floatValue();
+    }
+
+    public double Double() {
+        return ((Double)input[i++]).doubleValue();
+    }
+
+    public char Character() {
+        return ((Character)input[i++]).charValue();
+    }
+
+    public byte Byte() {
+        return ((Byte)input[i++]).byteValue();
+    }
+
+    public Object Object(String type) {
+        return input[i++];
+    }
+
+    public Object ObjectShallow(String type) {
+        return input[i++];
+    }
+    public void test1(){
+        i=0;
+        input = new Object[9];
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        i=0;
+        cute.Cute.input = this;
+        tests.BSTree.main(null);
+    }
+
+    public void test2(){
+        i=0;
+        input = new Object[9];
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(1);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        i=0;
+        cute.Cute.input = this;
+        tests.BSTree.main(null);
+    }
+
+    public void test6(){
+        i=0;
+        input = new Object[9];
+        input[i++] = new Integer(1);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(1);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(1);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(1);
+        input[i++] = new Integer(1);
+        input[i++] = new Integer(0);
+        i=0;
+        cute.Cute.input = this;
+        tests.BSTree.main(null);
+    }
+
+    public void test7(){
+        i=0;
+        input = new Object[9];
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(1);
+        input[i++] = new Integer(1);
+        i=0;
+        cute.Cute.input = this;
+        tests.BSTree.main(null);
+    }
+
+    public void test9(){
+        i=0;
+        input = new Object[9];
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(1);
+        input[i++] = new Integer(1);
+        input[i++] = new Integer(1);
+        i=0;
+        cute.Cute.input = this;
+        tests.BSTree.main(null);
+    }
+
+    public void test15(){
+        i=0;
+        input = new Object[9];
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(0);
+        input[i++] = new Integer(2);
+        input[i++] = null;
+        i=0;
+        cute.Cute.input = this;
+        tests.BSTree.main(null);
+    }
+
+}
